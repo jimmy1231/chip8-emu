aa29da811982da2b04fe30d8cbfdd6e24bb0f388
jimmy1231 <dfjimmy.li@gmail.com>
2026-01-02 14:38:36 -0500

    implement display - untested

--- chip8.go
+++ chip8.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"math/rand/v2"
 	"os"
 )
 
@@ -28,6 +29,10 @@ const PROGRAM = "roms/zero-demo.ch8"
 const ADDR_PROGRAM_START = 0x1FF
 const ADDR_FONT_START = 0x050
 const ADDR_FONT_END = 0x09F
+const ADDR_DISPLAY_START = 0xf00
+
+const DISPLAY_WIDTH = uint8(64)
+const DISPLAY_HEIGHT = uint8(32)
 
 const IS_DEBUG = true
 
@@ -46,7 +51,12 @@ var R_PC uint16
 // - In the draw instruction VF is set upon pixel collision
 var R_V [16]uint8
 
+var SP uint16
+
 const INDEX_VF = 15
+
+// hardware
+var PRESSED_KEY uint8
 
 func read_hbyte(value uint16, position int) uint8 {
 	position = min(max(position, 0), 3)
@@ -58,8 +68,181 @@ func read_byte(value uint16, position int) uint8 {
 	return uint8(value >> (position * 8))
 }
 
+func get_next(pc *uint16) uint16 {
+	return *pc + 2
+}
+
 func next(pc *uint16) {
-	*pc += 2
+	*pc = get_next(pc)
+}
+
+func key() uint8 {
+	return PRESSED_KEY & 0x0f
+}
+
+// blocks until key is pressed
+func get_key() uint8 {
+	// TODO
+	return 0x0
+}
+
+func get_delay() uint8 {
+	// TODO
+	return 0x0
+}
+
+func set_delay(value uint8) {
+	// TODO
+}
+
+func set_sound(value uint8) {
+	// TODO
+}
+
+func font_addr(c uint8) uint16 {
+	return ADDR_FONT_START + uint16(c&0x0f)*5
+}
+
+func get_bit(b uint8, offset uint8) uint8 {
+	return (b >> offset) & 0x1
+}
+
+func get_display_addr(x uint8, y uint8) (uint8, uint8) {
+	return (x / 8) * y, uint8(x % 8)
+}
+
+func get_pixel(m []uint8, x uint8, y uint8) uint8 {
+	addr, offset := get_display_addr(x, y)
+	return get_bit(m[addr], offset)
+}
+
+func set_pixel(m []uint8, x uint8, y uint8, value bool) {
+	addr, offset := get_display_addr(x, y)
+	if !value {
+		// unset
+		m[addr] &= ^(uint8(1) << offset)
+	} else {
+		// set
+		m[addr] |= (uint8(1) << offset)
+	}
+}
+
+/*
+Draws a sprite at coordinate start_x, start_y that has a width of 8 pixels
+and a height of 'height' pixels.
+
+Each row of 8 pixels is read as bit-coded starting from memory location 'sprite_addr';
+
+Returns
+- true if any screen pixels are flipped from set to unset when the sprite is drawn
+- false otherwise
+*/
+func draw(mem []uint8, start_x uint8, start_y uint8, height uint8, sprite_addr uint16) bool {
+	/*
+		display
+			y
+			^
+		5 |
+		4 |
+		3 |
+		2 |
+		1 |
+		0 |----------------------P
+			_ _ _ _ _ _ _ _ _ _ _  |_  _  _  _  _  _ > x
+			0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
+			|-------------| |-------------------|  |---->
+				b0                 b1                b2 ...
+
+		P is at b1, with bit offset of 3
+
+		memory
+		0xfff
+		...
+		|_________________|
+		|_________________|
+		|_________________|
+		|_________________|b2
+		|_________________|b1
+		|_________________|b0
+		|                 |
+		|                 |
+		msb				  lsb
+		...
+		0x000
+	*/
+	display := mem[ADDR_DISPLAY_START:]
+	sprite := mem[sprite_addr : sprite_addr+uint16(height)]
+
+	is_collision := false
+
+	for y := start_y; y < start_y+height; y++ {
+		for x := start_x; x < start_x+8; x++ {
+			display_pixel := get_pixel(display, x, y)
+			sprite_pixel := get_pixel(sprite, x-start_x, y-start_y)
+
+			// only modify display pixel if sprite pixel is set
+			if sprite_pixel == 0x1 {
+				if display_pixel == 0x1 {
+					set_pixel(display, x, y, false)
+					is_collision = true
+				} else {
+					// set display pixel
+					set_pixel(display, x, y, true)
+				}
+			}
+		}
+	}
+
+	disp_print(mem)
+	return is_collision
+}
+
+func disp_print(mem []uint8) {
+	if !IS_DEBUG {
+		return
+	}
+
+	display := mem[ADDR_DISPLAY_START:]
+
+	fmt.Println("------------- print display -------------")
+	for y := int(DISPLAY_HEIGHT) - 1; y >= 0; y-- {
+		fmt.Printf("%d:\t", y)
+		for x := range DISPLAY_WIDTH {
+			display_pixel := get_pixel(display, x, uint8(y))
+			if display_pixel == 0x1 {
+				fmt.Print(" * ")
+			} else {
+				fmt.Print("   ")
+			}
+		}
+		fmt.Printf("\n")
+	}
+	fmt.Print("\t")
+	for x := range DISPLAY_WIDTH {
+		fmt.Printf("%02d ", x)
+	}
+	fmt.Println("\n------------- end print display -------------")
+}
+
+func disp_clear(mem []uint8) {
+	for addr := 0xf00; addr <= 0xfff; addr++ {
+		mem[addr] = 0
+	}
+}
+
+func stack_push(mem []uint8, sp *uint16, value uint16) {
+	*sp += 2
+	b1, b0 := read_byte(value, 1), read_byte(value, 0)
+	// follow big endian
+	mem[*sp] = b1
+	mem[*sp+1] = b0
+}
+
+func stack_pop(mem []uint8, sp *uint16) uint16 {
+	b1, b0 := mem[*sp], mem[*sp+1]
+	*sp -= 2
+	// follow big endian
+	return (uint16(b1) << 8) | uint16(b0)
 }
 
 func main() {
@@ -140,7 +323,6 @@ func main() {
 	for {
 		// fetch
 		instruction := (uint16(mem[R_PC]) << 8) | uint16(mem[R_PC+1])
-		instruction = 0x8235
 		next(&R_PC)
 
 		// instruction:
@@ -149,7 +331,10 @@ func main() {
 		// 15 14 13 12 11 10  9  8  7  6  5  4  3  2  1  0
 		// |---------| |---------|  |--------|  |--------|
 		//     h3  			h2          h1          h0
+		//             |---------------------------------|
+		//                             NNN
 		b1, b0 := read_byte(instruction, 1), read_byte(instruction, 0)
+		NNN := instruction & 0x0fff
 
 		h3, h2, h1, h0 :=
 			read_hbyte(instruction, 3),
@@ -164,18 +349,26 @@ func main() {
 
 		if instruction == 0x00E0 {
 			// 00E0
+			disp_clear(mem)
 
 		} else if instruction == 0x00EE {
 			// 00E0
+			R_PC = stack_pop(mem, &SP)
 
 		} else if instruction != 0 && h3 == 0x0 {
 			// 0NNN
+			// set return addr to next instruction
+			stack_push(mem, &SP, R_PC)
+			R_PC = NNN
 
 		} else if h3 == 0x1 {
 			// 1NNN
+			R_PC = NNN
 
 		} else if h3 == 0x2 {
 			// 2NNN
+			stack_push(mem, &SP, R_PC)
+			R_PC = NNN
 
 		} else if h3 == 0x3 {
 			// 3XNN
@@ -243,8 +436,6 @@ func main() {
 			// 8XY5
 			VX := &R_V[h2]
 			VY := &R_V[h1]
-			*VX = 100
-			*VY = 120
 			VF := &R_V[INDEX_VF]
 
 			// set VF to 1 if no underflow
@@ -259,63 +450,143 @@ func main() {
 
 		} else if h3 == 0x8 && h0 == 0x6 {
 			// 8XY6
+			VX := &R_V[h2]
+			VY := &R_V[h1]
+			VF := &R_V[INDEX_VF]
+
+			*VX = *VY
+			*VF = *VX & 0x1
+			*VX >>= 1
 
 		} else if h3 == 0x8 && h0 == 0x7 {
 			// 8XY7
+			VX := &R_V[h2]
+			VY := &R_V[h1]
+			VF := &R_V[INDEX_VF]
+
+			if *VY >= *VX {
+				*VF = 1
+			} else {
+				*VF = 0
+			}
+
+			*VX = *VY - *VX
 
 		} else if h3 == 0x8 && h0 == 0xE {
 			// 8XYE
+			VX := &R_V[h2]
+			VY := &R_V[h1]
+			VF := &R_V[INDEX_VF]
+
+			*VX = *VY
+			*VF = (*VX >> 7) & 0x1
+			*VX <<= 1
 
 		} else if h3 == 0x9 {
 			// 9XY0
+			VX := &R_V[h2]
+			VY := &R_V[h1]
+
+			if *VX != *VY {
+				next(&R_PC)
+			}
 
 		} else if h3 == 0xA {
 			// ANNN
+			R_I = NNN
 
 		} else if h3 == 0xB {
 			// BNNN
+			R_PC = uint16(R_V[0]) + NNN
 
 		} else if h3 == 0xC {
 			// CXNN
+			VX := &R_V[h2]
+			NN := b0
+
+			*VX = uint8(rand.IntN(255)) & NN
 
 		} else if h3 == 0xD {
 			// DXYN
+			VX := &R_V[h2]
+			VY := &R_V[h1]
+			VF := &R_V[INDEX_VF]
+			N := h0
 
-		} else if h3 == 0xE {
-			// EX9E
+			is_collision := draw(mem, *VX, *VY, N, R_I)
+			if is_collision {
+				*VF = 1
+			} else {
+				*VF = 0
+			}
 
 		} else if h3 == 0xE && b0 == 0x9E {
-			// EXA1
+			// EX9E
+			VX := &R_V[h2]
+
+			if key() == *VX {
+				next(&R_PC)
+			}
 
 		} else if h3 == 0xE && b0 == 0xA1 {
-			// FX0A
+			// EXA1
+			VX := &R_V[h2]
+
+			if key() != *VX {
+				next(&R_PC)
+			}
 
 		} else if h3 == 0xF && b0 == 0x0A {
 			// FX0A
+			VX := &R_V[h2]
+			*VX = get_key()
 
 		} else if h3 == 0xF && b0 == 0x1E {
 			// FX1E
+			VX := &R_V[h2]
+			R_I += uint16(*VX)
 
 		} else if h3 == 0xF && b0 == 0x07 {
 			// FX07
+			VX := &R_V[h2]
+			*VX = get_delay()
 
 		} else if h3 == 0xF && b0 == 0x15 {
 			// FX15
+			VX := &R_V[h2]
+			set_delay(*VX)
 
 		} else if h3 == 0xF && b0 == 0x18 {
 			// FX18
+			VX := &R_V[h2]
+			set_sound(*VX)
 
 		} else if h3 == 0xF && b0 == 0x29 {
 			// FX29
+			VX := &R_V[h2]
+			R_I = font_addr(*VX)
 
 		} else if h3 == 0xF && b0 == 0x33 {
 			// FX33
+			VX := &R_V[h2]
+
+			hundreds := *VX / 100
+			tens := (*VX % 100) / 10
+			ones := *VX % 10
+
+			mem[R_I] = hundreds
+			mem[R_I+1] = tens
+			mem[R_I+2] = ones
 
 		} else if h3 == 0xF && b0 == 0x55 {
 			// FX55
+			VX := &R_V[h2]
+			copy(mem[R_I:], R_V[:*VX+1])
 
 		} else if h3 == 0xF && b0 == 0x65 {
 			// FX65
+			VX := &R_V[h2]
+			copy(R_V[:*VX+1], mem[R_I:])
 
 		} else {
 			panic("Incorrect opcode!")
